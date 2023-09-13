d2bd1aacc36e73d18f5a48f6d6c8da363ac1477b
Mgtantheta <freecityNJ@gmail.com>
2023-09-13 16:45:26 +0900

    HomeScreenに表示するデータを追加し、Viewに反映させた.

--- app/src/main/java/com/example/syllabusforsofties/home_screen/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/syllabusforsofties/home_screen/HomeScreenViewModel.kt
@@ -3,5 +3,40 @@ package com.example.syllabusforsofties.home_screen
 import androidx.lifecycle.ViewModel
 
 class HomeScreenViewModel: ViewModel() {
+    val courseThumbnailName = mutableListOf(
+        // 一限
+        "",
+        "",
+        "",
+        "",
+        "",
 
+        // 二限
+        "自然言語処理\n分散システム実践論",
+        "統計解析Ⅱ",
+        "コース演習Ⅱ",
+        "メディア論\n数値計算の理論と実際",
+        "応用情報システム学",
+
+        //3限
+        "",
+        "",
+        "プロジェクト演習Ⅱ",
+        "組込みシステム論",
+        "セキュリティ論",
+
+        // 4限
+        "",
+        "専門英語Ⅱ",
+        "",
+        "経営情報学",
+        "機械学習",
+
+        // 5限
+        "CG幾何学\n統合情報システム学Ⅰ",
+        "コース演習Ⅱ",
+        "",
+        "システムデザインPBL",
+        "マイクロコンピュータ制御"
+    )
 }
