3355ffff9f4e8977040c53a1819557a9b03d1add
Mgtantheta <freecityNJ@gmail.com>
2023-09-13 21:24:49 +0900

    教科名の見た目を見やすいように変更した

--- app/src/main/java/com/example/syllabusforsofties/home_screen/HomeScreen.kt
+++ app/src/main/java/com/example/syllabusforsofties/home_screen/HomeScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -78,7 +79,9 @@ fun GridCellItem(navController: NavController, courseName: String) {
     ) {
         Text(
             text = courseName,
-            modifier = Modifier.padding(4.dp)
+            fontSize = 13.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
         )
     }
 }
