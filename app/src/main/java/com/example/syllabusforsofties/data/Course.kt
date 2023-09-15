1549078c60bd4f5853a85e5ce64e77210a8d593e
Mgtantheta <freecityNJ@gmail.com>
2023-09-16 00:39:43 +0900

    データモデルの変数を変更

--- app/src/main/java/com/example/syllabusforsofties/data/Course.kt
+++ app/src/main/java/com/example/syllabusforsofties/data/Course.kt
@@ -3,7 +3,6 @@ package com.example.syllabusforsofties.data
 data class Course(
     val term: String?,
     val credits: String?,
-    val units: Int?,
     val coursePlan: Map<String, String>?,
     val textbook: String?,
     val gradingMethod: Map<String, String>?,
