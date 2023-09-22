0433b7bec5304d55e11b95257bb1910aa688ffa6
Mgtantheta <freecityNJ@gmail.com>
2023-09-22 15:23:17 +0900

    動的にデータを持ってきて表示できる機能を追加

--- app/src/main/java/com/example/syllabusforsofties/detail_screen/DetailScreen.kt
+++ app/src/main/java/com/example/syllabusforsofties/detail_screen/DetailScreen.kt
@@ -1,5 +1,6 @@
 package com.example.syllabusforsofties.detail_screen
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,6 +14,9 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -21,36 +25,54 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.syllabusforsofties.data.Course
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 @Composable
 fun DetailScreen(
     navController: NavController,
-    viewModel: DetailScreenViewModel
+    courseNamePram: String
 ) {
+    // HomeScreenからNavigateされたデータを取得する
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    val arguments = backStackEntry.value?.arguments
+    val courseName = arguments?.getString("courseName") ?: ""
 
-    val courseScheme = mutableListOf(
-        "間接型通信モデル",
-        "物理時計の同期問題",
-        "論理時計、イベントの順序付け",
-        "論理時計、イベントの順序付け",
-        "分散型排他制御",
-        "選任問題",
-        "複製に関する一貫性問題",
-        "複製に関する一貫性問題",
-        "複製に関する一貫性問題",
-        "トランザクション処理と同時実行制御",
-        "分散型トランザクション処理と同時実行制御",
-        "トランザクション処理における障害からの回復処理",
-        "トランザクション処理における障害からの回復処理",
-        "分散合意 - ２相コミットプロトコル",
-        "つなげ、まとめ"
-    )
+    val courseIDState = remember { mutableStateOf<String?>(null) }
+    val courseState = remember { mutableStateOf<Course?>(null) }
 
-    val courseObjectives = mutableListOf(
-        "理解度確認レポートと小テストの合格(6割)",
-        "理解度確認レポートと小テストの合格または演習課題の合格(8割以上)"
-    )
+    Log.d("Tag", "courseName: $courseName")
+
+    // courseIDを取得するコルーチンを起動
+    LaunchedEffect(key1 = courseName) {
+        val courseID = getCourseIDByCourseName(courseName = courseName)
+        courseIDState.value = courseID
+    }
+
+    // courseIDが取得できたら、courseを取得するコルーチンを起動
+    LaunchedEffect(key1 = courseIDState.value) {
+        val courseID = courseIDState.value
+        if (courseID != null) {
+            val course = getCourse(courseId = courseID)
+            Log.d("Tag", "course: $course")
+            courseState.value = course
+            Log.d("Tag", "courseState.courseName: ${courseState.value?.courseName}")
+        }
+    }
+    Log.d("Tag", "courseState.courseName2: ${courseState.value?.courseName}")
+
+    DetailScreenContent(course = courseState.value)
+}
+
+@Composable
+fun DetailScreenContent(course: Course?) {
+    // DocumentSnapshot to Course
 
     Column(
         modifier = Modifier
@@ -59,23 +81,23 @@ fun DetailScreen(
             .padding(16.dp), // パディングを追加
         verticalArrangement = Arrangement.spacedBy(8.dp) // テキスト間のスペースを追加
     ) {
-        PlanBox(title = "講義科目名", content = "分散システム実践論")
+        Log.d("Tag", "In DetailScreenContent ${course?.courseName}")
+        PlanBox(title = "講義科目名", content = course?.courseName ?: "")
 
-        CoursePlanBox(title = "授業の計画", array = courseScheme)
+        CoursePlanBox(title = "授業の計画", array = course?.coursePlan ?: listOf())
 
-        PlanBox(title = "配当年", content = "3年")
+        PlanBox(title = "配当年", content = course?.dividendYear ?: "")
 
-        PlanBox(title = "開講期間", content = "後期")
+        PlanBox(title = "開講期間", content = course?.term ?: "")
 
-        PlanBox(title = "単位数", content = "2")
+        PlanBox(title = "単位数", content = course?.credits ?: "")
 
-        PlanBox(title = "教科書", content = "特定のテキストは使用せず、適宜講義資料をファイル形式で配布する")
+        PlanBox(title = "教科書", content = course?.textbook ?: "")
 
-        CoursePlanBox(title = "目標", array = courseObjectives)
+        CoursePlanBox(title = "目標", array = course?.courseObjectives)
 
-        PlanBox(title = "担当教員", content = "王　家宏")
+        CoursePlanBox(title = "担当教員", array = course?.instructors ?: listOf())
     }
-
 }
 
 @Composable
@@ -105,7 +127,7 @@ fun PlanBox(title: String, content: String) {
 }
 
 @Composable
-fun CoursePlanBox(title: String, array: List<String>) {
+fun CoursePlanBox(title: String, array: List<String>?) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -120,7 +142,7 @@ fun CoursePlanBox(title: String, array: List<String>) {
                 style = MaterialTheme.typography.bodyMedium
             )
             // 1から15までのテキストを追加
-            array.forEachIndexed { index, content ->
+            array?.forEachIndexed { index, content ->
                 Text(
                     text = "${index + 1}: $content", // 講義内容に置き換えてください
                     style = MaterialTheme.typography.bodyMedium,
@@ -136,6 +158,66 @@ fun CoursePlanBox(title: String, array: List<String>) {
 @Preview(showBackground = true)
 @Composable
 fun DetailScreenPreview() {
-    DetailScreen(navController = rememberNavController(), DetailScreenViewModel())
+    DetailScreen(navController = rememberNavController(), courseNamePram = "")
 }
 
+suspend fun getCourseIDByCourseName(courseName: String): String? {
+    // Firebase Firestoreから対応するドキュメントを検索
+    val firestore = Firebase.firestore
+    val querySnapshot = firestore.collection("courses")
+        .whereEqualTo("講義科目名称", courseName)
+        .get()
+        .await()
+
+    // ドキュメントが見つかった場合、最初のドキュメントのcourseIDを取得
+    if (!querySnapshot.isEmpty) {
+        val document = querySnapshot.documents.first()
+        return document.id
+    }
+
+    // ドキュメントが見つからない場合はnullを返す
+    return null
+}
+
+//get course by courseID
+suspend fun getCourse(courseId: String): Course? {
+    return withContext(Dispatchers.IO) {
+        // Firestoreのコレクションにアクセスするための参照を作成
+        val db = Firebase.firestore
+        val docRef = db.collection("courses").document(courseId)
+
+        try {
+            val document = docRef.get().await()
+
+            if (document.exists()) {
+                val term: String? = document.getString("開講期間")
+                val credits: String? = document.getString("単位数")
+                val coursePlan: List<String>? = document.get("授業の計画") as List<String>?
+                val textbook: String? = document.getString("教科書")
+                val gradingMethod: List<String>? = document.get("成績評価の方法") as List<String>?
+                val courseName: String? = document.getString("講義科目名称")
+                val instructors: List<String>? = document.get("担当教員") as List<String>?
+                val courseObjective: List<String>? = document.get("到達目標") as List<String>?
+                val dividendYear: String? = document.getString("配当年")
+
+                Course(
+                    courseName = courseName,
+                    term = term,
+                    credits = credits,
+                    coursePlan = coursePlan,
+                    textbook = textbook,
+                    gradingMethod = gradingMethod,
+                    instructors = instructors,
+                    courseObjectives = courseObjective,
+                    dividendYear = dividendYear
+                )
+            } else {
+                Log.d("TAG", "No such document")
+                null
+            }
+        } catch (exception: Exception) {
+            Log.d("TAG", "get failed with $exception")
+            null
+        }
+    }
+}
--- app/src/main/java/com/example/syllabusforsofties/detail_screen/DetailScreenViewModel.kt
+++ app/src/main/java/com/example/syllabusforsofties/detail_screen/DetailScreenViewModel.kt
@@ -4,10 +4,12 @@ import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
 
 class DetailScreenViewModel : ViewModel() {
 
     private val TAG = "DetailScreenViewModel"
+
 
     // Firestore インスタンスを初期化
     val db = Firebase.firestore
@@ -16,23 +18,53 @@ class DetailScreenViewModel : ViewModel() {
     init {
         docRef.get()
             .addOnSuccessListener { document ->
-                onDocumentSuccess(document)
+                if (document != null) {
+                    Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+                } else {
+                    Log.d(TAG, "No such document")
+                }
             }
             .addOnFailureListener { exception ->
-                onDocumentFailure(exception)
+                Log.d(TAG, "get failed with ", exception)
             }
     }
 
-    private fun onDocumentSuccess(document: DocumentSnapshot?) {
-        if (document != null) {
-            Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+    // ここにFirestoreからデータを取得する処理を書く
+    fun getCourse(courseId: String): DocumentSnapshot? {
+        var course: DocumentSnapshot? = null
+        db.collection("courses").document(courseId)
+            .get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+                    course = document
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
 
-        } else {
-            Log.d(TAG, "No such document")
-        }
+        return course
     }
 
-    private fun onDocumentFailure(exception: Exception) {
-        Log.d(TAG, "get failed with ", exception)
+    // courseNameからcourseIdを取得する処理を書く
+    suspend fun getCourseIDByCourseName(courseName: String): String? {
+        // Firebase Firestoreから対応するドキュメントを検索
+        val firestore = Firebase.firestore
+        val querySnapshot = firestore.collection("courses")
+            .whereEqualTo("courseName", courseName)
+            .get()
+            .await()
+
+        // ドキュメントが見つかった場合、最初のドキュメントのcourseIDを取得
+        if (!querySnapshot.isEmpty) {
+            val document = querySnapshot.documents.first()
+            return document.id
+        }
+
+        // ドキュメントが見つからない場合はnullを返す
+        return null
     }
 }
