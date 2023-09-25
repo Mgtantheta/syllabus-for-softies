a71432ab8b08217a08b2b7d2d72a2f3c72a0bb3a
Mgtantheta <freecityNJ@gmail.com>
2023-09-25 19:22:30 +0900

    月曜~金曜と1限~5限を追加

--- app/src/main/java/com/example/syllabusforsofties/home_screen/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/syllabusforsofties/home_screen/HomeScreenViewModel.kt
@@ -4,7 +4,17 @@ import androidx.lifecycle.ViewModel
 
 class HomeScreenViewModel : ViewModel() {
     val courseThumbnailName = mutableListOf(
-        // 一限
+        // Week
+        "",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+
+        //
+        "1限\n8:50\n" +
+                "~\n10:20",
         "",
         "",
         "",
@@ -12,6 +22,8 @@ class HomeScreenViewModel : ViewModel() {
         "",
 
         // 二限
+        "2限\n10:30\n" +
+                "~\n12:00",
         listOf(
             "自然言語処理",
             "分散システム実践論",
@@ -25,6 +37,8 @@ class HomeScreenViewModel : ViewModel() {
         "応用情報システム学",
 
         //3限
+        "3限\n13:00\n" +
+                "~\n14:30",
         "",
         "",
         "プロジェクト演習Ⅱ",
@@ -32,6 +46,8 @@ class HomeScreenViewModel : ViewModel() {
         "セキュリティ論",
 
         // 4限
+        "4限\n14:40\n" +
+                "~\n16:10",
         "",
         "専門英語Ⅱ",
         "",
@@ -39,6 +55,8 @@ class HomeScreenViewModel : ViewModel() {
         "機械学習",
 
         // 5限
+        "5限\n16:20\n" +
+                "~\n17:50",
         listOf(
             "CG幾何学",
             "統合情報システム学Ⅰ",
@@ -50,7 +68,22 @@ class HomeScreenViewModel : ViewModel() {
             "マイクロコンピュータ制御",
             "情報環境論"
         )
-
     )
+
+    //遷移しないリスト
+    val notClickableList = mutableListOf<String>(
+        "",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "1限\n8:50~10:20",
+        "2限\n10:30~12:00",
+        "3限\n13:00~14:30",
+        "4限\n14:40~16:10",
+        "5限\n16:20~17:50",
+    )
+
 
 }
